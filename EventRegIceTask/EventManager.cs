f371febab2781d52d1d7eb8741c9d58f5baf9203
lab_services_student <lab_services_student@lab7L95SR>
2025-10-17 18:37:35 +0200

    Add project files.

---
+++ EventRegIceTask/EventManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventRegIceTask
+
+{
+    public class EventManager
+    {
+        // Private field to store attendees
+        private HashSet<string> attendees = new HashSet<string>();
+
+        // Adds a new attendee
+        public void AddAttendee(string name)
+        {
+            if (attendees.Add(name))
+            {
+                Console.WriteLine($"\"{name}\" has been registered successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"\"{name}\" is already registered.");
+            }
+        }
+
+        // Removes an attendee
+        public void RemoveAttendee(string name)
+        {
+            if (attendees.Remove(name))
+            {
+                Console.WriteLine($"\"{name}\" has been removed.");
+            }
+            else
+            {
+                Console.WriteLine($"\"{name}\" was not found in the registration list.");
+            }
+        }
+
+        // Displays all registered attendees
+        public void DisplayAllAttendees()
+        {
+            Console.WriteLine("\n--- Registered Attendees ---");
+            if (attendees.Count == 0)
+            {
+                Console.WriteLine("No one is registered yet.\n");
+                return;
+            }
+
+            foreach (var attendee in attendees)
+            {
+                Console.WriteLine(attendee);
+            }
+            Console.WriteLine(); // Blank line for spacing
+        }
+    }
+}
---
+++ EventRegIceTask/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EventRegIceTask
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventManager manager = new EventManager();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("\nEvent Attendee Manager");
+                Console.WriteLine("1. Add Attendee");
+                Console.WriteLine("2. Remove Attendee");
+                Console.WriteLine("3. Display All Attendees");
+                Console.WriteLine("4. Exit");
+                Console.Write("Please enter your choice: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Enter attendee name to add: ");
+                        string addName = Console.ReadLine();
+                        manager.AddAttendee(addName);
+                        break;
+
+                    case "2":
+                        Console.Write("Enter attendee name to remove: ");
+                        string removeName = Console.ReadLine();
+                        manager.RemoveAttendee(removeName);
+                        break;
+
+                    case "3":
+                        manager.DisplayAllAttendees();
+                        break;
+
+                    case "4":
+                        running = false;
+                        Console.WriteLine("Exiting program. Goodbye!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
